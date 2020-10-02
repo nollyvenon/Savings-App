02c94cdcab154c4b31c52f89d849fe3a590746fc
snrweb <Awelewa_tobi@yahoo.com>
2020-10-02 14:38:08 +0100

    added splash screen

--- app/src/main/java/com/johnnyup/erssavingsapp/LoginActivity.java
+++ app/src/main/java/com/johnnyup/erssavingsapp/LoginActivity.java
@@ -50,6 +50,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
